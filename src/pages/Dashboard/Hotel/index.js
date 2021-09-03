d23e4648c918e5f59ed4e9554c15a0e469ebf7b6
Vitor Lourenco <vitor.registros@lourencos.net>
2021-09-03 10:28:08 -0300

    feat: rejection logic for no payment

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,13 +1,36 @@
 import Reject from "./Reject";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import useApi from "../../../hooks/useApi";
+
+class Hotels {
+  constructor(error, hotels) {
+    this.error = error;
+    this.hotels = hotels;
+  }
+}
 
 export default function Hotel() {
-  const hasPaid = false;
-  const shouldReject = !hasPaid;
+  const api = useApi();
+  const [hotels, setHotels] = useState(new Hotels(null, []));
+
+  useEffect(() => {
+    api.hotel
+      .getHotelsByUser()
+      .then(({ data: hotels }) => {
+        setHotels(new Hotels(null, hotels));
+      })
+      .catch((err) => {
+        const message = err.response.data.message;
+        console.log(message);
+        setHotels(new Hotels(message, []));
+      });
+  }, []);
+
   return (
     <>
       <Header>Escolha de hotel e quarto</Header>
-      {shouldReject ? <Reject /> : <p>em breve</p>}
+      <Reject {...{ hotels }} />
     </>
   );
 }
