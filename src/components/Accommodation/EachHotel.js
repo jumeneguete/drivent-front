98a5e63121d595ae6d87d58dc2f18b2146c2864b
Laura <laura.tracierra@gmail.com>
2021-09-03 08:54:00 -0300

    feat: add choose hotel option

--- src/components/Accommodation/EachHotel.js
+++ src/components/Accommodation/EachHotel.js
@@ -1,13 +1,57 @@
 import styled from "styled-components";
-import Typography from "@material-ui/core/Typography";
-import Button from "../Form/Button";
 
-export default function EachHotel({ name, accommodationType, beds, image }) {
+export default function EachHotel({
+  name,
+  accommodationType,
+  beds,
+  image
+}) {
   return (
-    <Button>
+    <EachHotelContainer>
       <img src={image} />
       <h6>{name}</h6>
-      <p>Vagas disponíveis: {beds}</p>
-    </Button>
+      <p>
+        <strong>Tipos de acomodação: </strong>
+        <br />
+        {accommodationType.length === 1
+          ? accommodationType[0]
+          : accommodationType.length === 2
+            ? accommodationType[0] + " e " + accommodationType[1]
+            : accommodationType[0] +
+            ", " +
+            accommodationType[1] +
+            " e " +
+            accommodationType[2]}
+      </p>
+      <p>
+        <strong>Vagas disponíveis: </strong>
+        <br />
+        {beds}
+      </p>
+    </EachHotelContainer>
   );
 }
+
+const EachHotelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  text-transform: initial;
+
+  > h6 {
+    font-size: 20px;
+    margin: 10px 0;
+  }
+  >p {
+    font-size: 12px;
+    line-height: 14px;
+    text-align: start;
+    margin-bottom: 14px;
+  }
+  >img {
+    width: 168px !important;
+    height: 109px;
+    margin: auto;
+    border-radius: 5px;
+  }
+`;
--- src/components/Accommodation/index.js
+++ src/components/Accommodation/index.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import Typography from "@material-ui/core/Typography";
+import { useState } from "react";
 import Button from "../Form/Button";
+
 import EachHotel from "./EachHotel";
 
 export default function Accommodation() {
   const hotelsAvailables = [
     {
+      id: 1,
       name: "Driven Resort",
       acomodationsType: ["Single", "Double"],
       beds: 103,
@@ -13,6 +16,7 @@ export default function Accommodation() {
         "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg",
     },
     {
+      id: 2,
       name: "Driven Palace",
       acomodationsType: ["Single", "Double", "Triple"],
       beds: 25,
@@ -20,6 +24,7 @@ export default function Accommodation() {
         "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/f7/8b/38/exterior.jpg?w=900&h=-1&s=1",
     },
     {
+      id: 3,
       name: "Driven World",
       acomodationsType: ["Single", "Double"],
       beds: 2,
@@ -27,6 +32,8 @@ export default function Accommodation() {
         "https://imgcy.trivago.com/c_lfill,d_dummy.jpeg,e_sharpen:60,f_auto,h_450,q_auto,w_450/itemimages/96/95/96959_v6.jpeg",
     },
   ];
+  const [chosenHotel, setChosenHotel] = useState(0);
+  console.log(chosenHotel);
 
   return (
     <>
@@ -38,12 +45,15 @@ export default function Accommodation() {
       </StyledTypography>
       <HotelContainer>
         {hotelsAvailables.map((n) => (
-          <EachHotel
-            name={n.name}
-            accommodationType={n.acomodationsType}
-            beds={n.beds}
-            image={n.image}
-          />
+          <StyledButton onClick={() => setChosenHotel(n.id)} state={chosenHotel === n.id? true: false}>
+            <EachHotel
+              key={n.id}
+              name={n.name}
+              accommodationType={n.acomodationsType}
+              beds={n.beds}
+              image={n.image}
+            />
+          </StyledButton>
         ))}
       </HotelContainer>
     </>
@@ -51,22 +61,21 @@ export default function Accommodation() {
 }
 
 const StyledTypography = styled(Typography)`
-  margin-bottom: 20px !important;
+  margin-bottom: ${(props) =>
+    props.variant === "h6" ? "-18px" : "20px"} !important;
+  color: ${(props) => (props.variant === "h6" ? "#8E8E8E" : null)};
 `;
 
 const HotelContainer = styled.div`
   margin-top: 40px !important;
   width: 100% !important;
-
-  > button {
-    margin-top: 0 !important;
+  >button{
+    margin: 0 19px 0 0 !important;
     box-shadow: none;
-    background-color: #f1f1f1;
-    margin-right: 19px;
+    padding: 14px 16px 0px 16px!important;
   }
+`;
 
-  img{
-    width: 168px!important;
-    height: 109px;
-  }
+const StyledButton = styled(Button)`
+  background-color: ${(props) => (props.state? "#FFEED2" : "#f1f1f1")}!important;
 `;
