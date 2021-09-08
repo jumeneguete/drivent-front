57a4596afef76a85a8437cb42f5dc572b0b9439c
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-08 10:56:15 -0300

    feat: component for activities when user bought Online option

---
+++ src/components/Activities/OnlineActivities.js
@@ -0,0 +1,34 @@
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
+
+export default function OnlineActivities() {
+  return (
+    <>
+      <MessageContainer>
+        <Message> 
+        Sua modalidade de ingresso não necessita escolher <br />
+        atividade. Você terá acesso a todas as atividades.
+        </Message>
+      </MessageContainer>
+    </>
+  );
+}
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const MessageContainer = styled.div`
+  width: 100%;
+  min-height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Message = styled.h2`
+  color: #8E8E8E;
+  font-size: 20px;
+  text-align: center;
+  line-height: 25px;
+`;
